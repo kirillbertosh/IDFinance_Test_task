0e3d2eba2656d122b2d9fa4e28254c52bad9cd4d
Kirill Bertosh <kirillbertosh@gmail.com>
2018-06-26 18:28:27 +0300

    Added client controller

---
+++ src/main/java/com/idFinance/bertosh/controllers/ClientController.java
@@ -0,0 +1,34 @@
+package com.idFinance.bertosh.controllers;
+
+import com.idFinance.bertosh.model.Request;
+import com.idFinance.bertosh.repositories.RequestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/client/requests")
+public class ClientController {
+
+    @Autowired
+    private RequestRepository repository;
+
+    @GetMapping("/status/{id}")
+    public ResponseEntity findRequest(@PathVariable Long id) {
+        Optional<Request> request = repository.findById(id);
+        if (request.isPresent()) {
+            return new ResponseEntity<>(request.get().getStatus(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity save(@RequestBody Request request) {
+        request = repository.save(request);
+        return new ResponseEntity<>(request, HttpStatus.CREATED);
+    }
+}
